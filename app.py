51900506e1fdef2ffb5c4779c6e00e862ffba5bd
bennobot <ben@pigs-ears.co.uk>
2025-12-25 11:48:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def create_cin7_purchase_order(header_df, lines_df, location_choice):
             "TaskID": task_id,
             "CombineAdditionalCharges": False,
             "Memo": "Streamlit Import",
-            "Status": "DRAFT",
+            "Status": "Draft",
             "Lines": order_lines
         }
         try:
